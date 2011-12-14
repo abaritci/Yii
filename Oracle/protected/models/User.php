74144f25977fbf80db85b3f8d442e1d446bcd542
Isabel Palomar <isa_99960@hotmail.com>
2011-12-14 17:55:11 -0600

    Unique username/sort

--- Oracle/protected/models/User.php
+++ Oracle/protected/models/User.php
@@ -36,8 +36,12 @@ class User extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
+                        array('USERNAME, PASSWORD, EMAIL', 'required'),
 			array('ID', 'numerical'),
-			array('USERNAME, PASSWORD, EMAIL', 'length', 'max'=>128),
+			array('USERNAME, PASSWORD', 'length', 'max'=>128),
+                        array('USERNAME', 'unique', 'attributeName'=>'USERNAME'),
+
+                        array('EMAIL', 'email'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
 			array('ID, USERNAME, PASSWORD, EMAIL', 'safe', 'on'=>'search'),
@@ -79,6 +83,10 @@ class User extends CActiveRecord
 
 		$criteria=new CDbCriteria;
 
+               // $criteria->condition = "ID=1";
+                $sort=new CSort();
+                $sort->defaultOrder='USERNAME ASC';
+
 		$criteria->compare('ID',$this->ID);
 		$criteria->compare('USERNAME',$this->USERNAME,true);
 		$criteria->compare('PASSWORD',$this->PASSWORD,true);
@@ -86,6 +94,7 @@ class User extends CActiveRecord
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
+                        'sort'=>$sort,
 		));
 	}
 

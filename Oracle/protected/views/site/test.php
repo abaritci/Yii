ed8a34322db1344544faddcb71f7bca20f8aa8f0
Isabel Palomar <isa_99960@hotmail.com>
2011-12-16 14:39:27 -0600

    Examples datetimepicker

--- Oracle/protected/views/site/test.php
+++ Oracle/protected/views/site/test.php
@@ -4,8 +4,10 @@ $this->breadcrumbs=array(
 	'Test',
 );
 ?>
+
 <p>Just  examples :) </p>
 <div class="row" >
+        <?php echo 'Fecha'; ?>&nbsp;&nbsp;&nbsp;
         <?php
         $this->widget('zii.widgets.jui.CJuiDatePicker', array(
             'name'=>'publishDate',
@@ -39,5 +41,53 @@ $this->breadcrumbs=array(
         ));
         ?>
 </div>
+<br>
+<div class="row">
+    <?php echo "Teléfono"?>
+    <?php
+        $this->widget('CMaskedTextField', array(
+        'name'=>'telefono',
+        'attribute' => 'telefono_persona',
+        'mask' => '(9999)-999-9999',
+        'htmlOptions' => array(
+            'size' => 11,
+              'style'=>'width:150px;'
+            ),
+        ));
+        ?>
+    <?php echo 'Ejemplo de un CMaskedTextField'; ?>
+</div>
+<br>
+<div class="row">
+  <?php echo 'Fecha M'  ?>
+  <?php
+    $this->widget('CMaskedTextField', array(
+        'name'=>'fechaMask',
+        'mask'=>'99/99/9999',
+        'htmlOptions'=>array(
+            'size'=>8,
+            'style'=>'width:150px;'
+        ),
+        
+    ) );
+
+  ?>
+</div>
+<BR>
+<DIV class="row">
+    <?php echo "datetime";  ?>
+    <?php Yii::import('application.extensions.CJuiDateTimePicker.CJuiDateTimePicker');
+    $this->widget('CJuiDateTimePicker',array(
+        'name'=>'otro',
+        //'model'=>$news, //Model object
+        'attribute'=>'eventDate', //attribute name
+                'mode'=>'datetime', //use "time","date" or "datetime" (default)
+        'options'=>array() // jquery plugin options
+    ));
+?>
+
+</DIV>
+
+
 
 
